5e49bf7d81bea253cdd5e7d5e29a55ad912c37c3
Brian Sturgis <sturujisu@gmail.com>
2021-03-29 11:02:49 -0700

    this will create Treat model

---
+++ Models/Treat.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+
+namespace PierreTreats.Models
+{
+  public class Treat
+  {
+    public Treat()
+    {
+       this.JoinEntities = new HashSet<FlavorTreat>();
+
+    }
+
+    public int TreatId {get; set;}
+
+    public string TreatName {get; set;}
+
+    public string TreatDescription {get;set;}
+
+    public virtual ApplicationUser User { get; set; }
+
+    public virtual ICollection<RecipeTag> JoinEntities { get;}
+
+  }
+
+}
