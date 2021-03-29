891f41913d78955eec9d411eed14ff84d28df7b2
Brian Sturgis <sturujisu@gmail.com>
2021-03-29 15:10:31 -0700

    update project with flavor routes and views dor deletions but is curently broken

--- PierreTreats/Controllers/TreatsController.cs
+++ PierreTreats/Controllers/TreatsController.cs
@@ -44,7 +44,7 @@ namespace PierreTreats.Controllers
       var currentUser = await _userManager.FindByIdAsync(userId);
       treat.User = currentUser;
       _db.Treats.Add(treat);
-      _db.SaveChanges();
+      
       if (FlavorId != 0)
       {
         _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
@@ -76,6 +76,21 @@ namespace PierreTreats.Controllers
       {
       _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
       }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      _db.Treats.Remove(thisTreat);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
