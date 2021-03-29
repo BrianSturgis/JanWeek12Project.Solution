5a869b5391bbe87b8053f80f87ac731e202f7737
Brian Sturgis <sturujisu@gmail.com>
2021-03-29 13:59:50 -0700

    update project with falvor controller, index route, and index view

---
+++ PierreTreats/Controllers/FlavorsController.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using PierreTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace PierreTreats.Controllers
+{
+  [Authorize]
+  public class FlavorsController  :  Controller
+  {
+      private readonly PierreTreatsContext _db;
+
+      private readonly UserManager<ApplicationUser> _userManager;
+
+      public FlavorsController(UserManager<ApplicationUser> userManager, PierreTreatsContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public async Task<ActionResult> Index()
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      var userFlavors = _db.Flavors.Where(entry => entry.User.Id == currentUser.Id).ToList();
+      return View(userFlavors);
+    }
+
+    
+
+
+  }
+
+
+}
--- PierreTreats/Controllers/TreatsController.cs
+++ PierreTreats/Controllers/TreatsController.cs
@@ -62,6 +62,24 @@ namespace PierreTreats.Controllers
       return View(thisTreat);
     }
 
+    public ActionResult AddFlavor(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorDescription");
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+    public ActionResult AddFlavor(Treat treat, int FlavorId)
+    {
+      if (FlavorId != 0)
+      {
+      _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
   }
 
 }
